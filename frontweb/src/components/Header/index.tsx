b8c6bb6cd3658dd75c9e11c6464809dac49f935f
Romário Ferreira <romarioferreirarezende@hotmail.com>
2023-01-19 10:51:50 -0300

    customizacao do tema do bootstrap

--- frontweb/src/components/Header/index.tsx
+++ frontweb/src/components/Header/index.tsx
@@ -3,7 +3,7 @@ import "./styles.css";
 const Header = () => {
   return (
     <div className="header-container">
-      <div className="title-app">
+      <div className="title-app ">
         <h6>Carros Top</h6>
       </div>
     </div>
