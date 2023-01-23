015651065a33c9a9e9109a2764d2a7082db48de5
Romário Ferreira <romarioferreirarezende@hotmail.com>
2023-01-23 15:48:44 -0300

    estilizando a navbar

--- frontweb/src/components/Header/index.tsx
+++ frontweb/src/components/Header/index.tsx
@@ -1,6 +1,6 @@
 import './styles.css';
 import '@popperjs/core';
-import 'bootstrap/js/src/collapse.js';
+import 'bootstrap/js/src/collapse';
 import { Link, NavLink } from 'react-router-dom';
 
 const Header = () => {
@@ -8,23 +8,23 @@ const Header = () => {
     <nav className="navbar navbar-expand-md navbar-dark bg-primary main-nav">
       <div className="container-fluid">
         <Link to="/home" className="nav-logo-text">
-          <h4 className='title-app'>Carros Top</h4>
+          <h4>Carros Top</h4>
         </Link>
 
         <button
           className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
-          data-bs-target="#dscatalog-navbar"
-          aria-controls="dscatalog-navbar"
+          data-bs-target="#carrostop-navbar"
+          aria-controls="carrostop-navbar"
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
 
-        <div className="collapse navbar-collapse" id="dscatalog-navbar">
-          <ul className="navbar-nav offset-md-2 main-menu">
+        <div className="collapse navbar-collapse" id="carrostop-navbar">
+          <ul className="navbar-nav offset-md-8 main-menu">
             <li>
               <NavLink to="/home">HOME</NavLink>
             </li>
