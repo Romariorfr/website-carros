db25050e78175c1515ff3ecb9d74b6c10adf262d
Romário Ferreira <romarioferreirarezende@hotmail.com>
2023-01-24 10:44:01 -0300

    estilizando o card do homepage

--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -1,8 +1,18 @@
-const Home = () =>{
-    return(
-        
-        <h1>Home page new</h1>
-    )
-}
+import './styles.css';
+import { ReactComponent as Car } from '../../assets/images/car.svg';
+
+const Home = () => {
+  return (
+    <>
+      <div className="home-container">
+        <div className="card-container">
+          <Car />
+          <h1>O carro perfeito para você</h1>
+          <h4>Conheça nossos carros e dê mais um passo na realização do seu sonho</h4>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Home;
