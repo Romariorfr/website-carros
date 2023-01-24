45e4d78fe86d5a99ff86ddd9065d4f2aeb2c463f
Romário Ferreira <romarioferreirarezende@hotmail.com>
2023-01-24 10:00:21 -0300

    adequado as versoes do projeto

--- frontweb/src/index.tsx
+++ frontweb/src/index.tsx
@@ -1,18 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import '@popperjs/core'
-import 'bootstrap/js/dist/collapse';
+import ReactDOM from 'react-dom';
 import App from './App';
-
-
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
