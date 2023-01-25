1245c321fb26387fd76bddf5f17160165e54726e
Romário Ferreira <romarioferreirarezende@hotmail.com>
2023-01-24 23:39:42 -0300

    criado card-catalog

--- frontweb/src/pages/Home/Catalogo/index.tsx
+++ frontweb/src/pages/Home/Catalogo/index.tsx
@@ -1,7 +1,21 @@
-const Catalogo = () =>{
-    return(
-        <div>pagina catalogo</div>
-    )
-}
+import './styles.css';
+import { ReactComponent as CarRed } from '../../../assets/images/car-red.svg';
+
+const Catalogo = () => {
+  return (
+    <div className="catalog-container">
+      <div className="card-buscar">
+        <input type="text" />
+        <button>BUSCAR</button>
+      </div>
+      <div className="card-product">
+        <CarRed />
+        <h1>Audi Supra TT</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, nisi</p>
+        <button>COMPRAR</button>
+      </div>
+    </div>
+  );
+};
 
 export default Catalogo;
