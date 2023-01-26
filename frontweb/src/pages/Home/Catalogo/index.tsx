79ad792734fcfef6a9f1a78fca99b71fafba95cb
Romário Ferreira <romarioferreirarezende@hotmail.com>
2023-01-26 09:46:26 -0300

    add lista de carro estatica

--- frontweb/src/pages/Home/Catalogo/index.tsx
+++ frontweb/src/pages/Home/Catalogo/index.tsx
@@ -14,6 +14,62 @@ const Catalogo = () => {
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, nisi</p>
         <button>COMPRAR</button>
       </div>
+
+      <div className="card-product">
+        <CarRed />
+        <h1>Audi Supra TT</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, nisi</p>
+        <button>COMPRAR</button>
+      </div>
+
+      <div className="card-product">
+        <CarRed />
+        <h1>Audi Supra TT</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, nisi</p>
+        <button>COMPRAR</button>
+      </div>
+
+      <div className="card-product">
+        <CarRed />
+        <h1>Audi Supra TT</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, nisi</p>
+        <button>COMPRAR</button>
+      </div>
+
+      <div className="card-product">
+        <CarRed />
+        <h1>Audi Supra TT</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, nisi</p>
+        <button>COMPRAR</button>
+      </div>
+
+      <div className="card-product">
+        <CarRed />
+        <h1>Audi Supra TT</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, nisi</p>
+        <button>COMPRAR</button>
+      </div>
+
+      <div className="card-product">
+        <CarRed />
+        <h1>Audi Supra TT</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, nisi</p>
+        <button>COMPRAR</button>
+      </div>
+
+      <div className="card-product">
+        <CarRed />
+        <h1>Audi Supra TT</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, nisi</p>
+        <button>COMPRAR</button>
+      </div>
+
+      <div className="card-product">
+        <CarRed />
+        <h1>Audi Supra TT</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, nisi</p>
+        <button>COMPRAR</button>
+      </div>
     </div>
   );
 };
